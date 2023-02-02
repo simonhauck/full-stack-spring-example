4bf42d52b0ae53ef76339b1a18e756faa77923db
Simon Hauck <simon.hauck@gmx.de>
2023-02-02 22:24:29 +0100

    #1 add open api code generation

---
+++ server-api/build.gradle.kts
@@ -0,0 +1,3 @@
+plugins {
+    id("com.github.simonhauck.example.java-conventions")
+}
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
 	id("com.github.simonhauck.example.artifactory")
 	id("com.github.simonhauck.example.kotlin-conventions")
 	id("com.github.simonhauck.example.spring-conventions")
+
+	// Generate open api doc
+	id("org.springdoc.openapi-gradle-plugin") version "1.6.0"
 }
 
 dependencies {
@@ -11,6 +14,23 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
+	// OpenApi Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
+// User another port to have it not clashing with running instances
+openApi {
+	val apiGeneratedPort = 59186
+	apiDocsUrl.set("http://localhost:$apiGeneratedPort/v3/api-docs/openapi.json")
+	outputDir.set(file("${project(":server-api").projectDir}/src/main/resources"))
+
+	customBootRun {
+		args.set(
+			listOf(
+				"--server.port=$apiGeneratedPort",
+			)
+		)
+	}
+}
