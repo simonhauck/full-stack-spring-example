87dd5f4eb0fc34da59d8c6f6f3b5532322e6c945
Simon Hauck <simon.hauck@gmx.de>
2023-02-10 13:25:27 +0100

    #5 try with static image name

--- buildSrc/src/main/kotlin/com.github.simonhauck.example.docker.gradle.kts
+++ buildSrc/src/main/kotlin/com.github.simonhauck.example.docker.gradle.kts
@@ -1,10 +1,9 @@
-plugins {
-    id("com.google.cloud.tools.jib")
-}
+plugins { id("com.google.cloud.tools.jib") }
 
 jib {
     to {
-        val registry = System.getenv("DOCKER_REGISTRY")?.plus("/") ?: ""
+        //        val registry = System.getenv("DOCKER_REGISTRY")?.plus("/") ?: ""
+        val registry = "shauck/"
         val branchName = System.getenv("BRANCH_NAME") ?: "unknown"
         image = "$registry${project.rootProject.name}/${project.name}"
         tags = setOf("${project.version}", branchName)
