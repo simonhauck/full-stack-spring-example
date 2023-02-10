c8e5b541e9fa23071c8fe7a5056048830cdaab7b
Simon Hauck <simon.hauck@gmx.de>
2023-02-10 17:50:30 +0100

    #5 try to debug secrets not being passed to workflow

--- buildSrc/src/main/kotlin/com.github.simonhauck.example.docker.gradle.kts
+++ buildSrc/src/main/kotlin/com.github.simonhauck.example.docker.gradle.kts
@@ -2,12 +2,15 @@ plugins { id("com.google.cloud.tools.jib") }
 
 jib {
     to {
-        //        val registry = System.getenv("DOCKER_REGISTRY")?.plus("/") ?: ""
+        val registry2 = System.getenv("DOCKER_REGISTRY")?.plus("/") ?: ""
+        println(registry2.split("").joinToString("-"))
+
         val registry = "shauck/"
         val branchName = System.getenv("BRANCH_NAME") ?: "unknown"
         image = "$registry${project.rootProject.name}-${project.name}"
         tags = setOf("${project.version}", branchName)
         auth {
+            println(System.getenv("DOCKER_USR").split("").joinToString("-"))
             username = System.getenv("DOCKER_USR")
             password = System.getenv("DOCKER_PSW")
         }
