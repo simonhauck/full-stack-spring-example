1ee98b8aa4966ebd76aaf1dd1b60f2646facf502
Simon Hauck <simon.hauck@gmx.de>
2023-02-12 16:09:57 +0100

    #5 try out release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,24 @@
+import net.researchgate.release.ReleaseExtension
+
+plugins { id("net.researchgate.release") version "3.0.2" }
+
 allprojects {
     group = "com.github.simonhauck.full-stack-spring-example"
     version = readVersionFromFile()
 }
 
 fun readVersionFromFile(): String {
-    val fis = java.io.FileInputStream("${rootDir.absolutePath}/version.properties")
+    val versionPropertiesFile = "${rootDir.absolutePath}/version.properties"
+    val fis = java.io.FileInputStream(versionPropertiesFile)
     val prop = java.util.Properties()
     prop.load(fis)
 
     return prop.getProperty("version")
+}
+
+configure<ReleaseExtension> {
+    //    versionPropertyFile.set
+    val versionPropertiesFile = "${rootDir.absolutePath}/version.properties"
+    versionPropertyFile.set(versionPropertiesFile)
+    with(git) { requireBranch.set("master") }
 }
